51898dce69bd1550d3ac2b65c6a9c81f51a660f9
ubuntu <vm@exemple.com>
2026-05-26 14:35:03 +0000

    multicast p2 done

---
+++ README.md
@@ -0,0 +1,4 @@
+ip a
+ip route
+bridge fdb show dev vxlan10
+/sbin/ip -d link show vxlan10
---
+++ p2/multicast/router2/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "Waiting for GNS3 to plug eth1..."
+while ! ip link show dev eth1 >/dev/null 2>&1; do
+    sleep 0.5
+done
+sleep 1
+
+ip addr add 10.0.0.2/24 dev eth1
+ip link set dev eth1 up
+
+ip link add br0 type bridge
+ip link set dev br0 up
+ip link set dev eth0 up
+ip link set dev eth0 master br0
+
+ip route add 224.0.0.0/4 dev eth1
+ip link add vxlan10 type vxlan id 10 group 239.1.1.1 dstport 4789 dev eth1
+ip link set dev vxlan10 up
+ip link set dev vxlan10 master br0
+
+echo "Multicast VXLAN on R1 deployed successfully!"
+
+/usr/lib/frr/frrinit.sh start
+exec tail -f /dev/null
